1d0edd7b6ee231d8fd865e207c32bcf221a3748e
Ramesh Mane <101566080+rameshmane7218@users.noreply.github.com>
2024-05-07 06:52:19 +0000

    fix(nc-gui): add config validators

--- packages/nc-gui/composables/useSharedFormViewStore.ts
+++ packages/nc-gui/composables/useSharedFormViewStore.ts
@@ -324,7 +324,7 @@ const [useProvideSharedFormStore, useSharedFormStore] = useInjectionState((share
       rules = [...rules, ...additionalRules]
 
       if (rules.length) {
-        rulesObj[column.title] = rules
+        rulesObj[column.title!] = rules
       }
     }
 
